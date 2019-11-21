ed169262be94b13ac22ce7b97c34c21eea3ceac7
Darjan Pezer <darjan.pezer@agiledrop.com>
2019-11-21 12:59:56 +0100

    Npm updates.

--- npm/gulp/tasks/styles.js
+++ npm/gulp/tasks/styles.js
@@ -12,15 +12,15 @@
 
 module.exports = function(gulp, $, config, messages) {
   gulp.task('styles', function() {
-  	return gulp.src(config.sass.src)
-  		.pipe($.plumber({
-  			errorHandler: messages.error
-  		}))
-  		.pipe($.sass(config.sass.config))
-  		.pipe($.autoprefixer({
-  			browsers: config.sass.autoprefixer
-  		}))
-  		.pipe(gulp.dest(config.sass.destination))
-  		.pipe($.notify(messages.success));
+    return gulp.src(config.sass.src)
+      .pipe($.plumber({
+        errorHandler: messages.error
+      }))
+      .pipe($.sass(config.sass.config))
+      .pipe($.autoprefixer({
+        browsers: config.sass.autoprefixer
+      }))
+      .pipe(gulp.dest(config.sass.destination))
+      .pipe($.notify(messages.success));
   });
 };
